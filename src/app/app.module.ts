0bcda753e2807e990ef1b620d8b8d72f7fbc3ac2
Prasath Muthaiyan <prasath.muthaiyan@gmail.com>
2018-06-23 11:03:03 +0530

    Tested in ios 10.3.3 working

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,30 +1,46 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { ErrorHandler, NgModule } from '@angular/core';
-import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
-import { SplashScreen } from '@ionic-native/splash-screen';
-import { StatusBar } from '@ionic-native/status-bar';
+import {BrowserModule} from '@angular/platform-browser';
+import {ErrorHandler, NgModule} from '@angular/core';
+import {IonicApp, IonicErrorHandler, IonicModule} from 'ionic-angular';
+import {SplashScreen} from '@ionic-native/splash-screen';
+import {StatusBar} from '@ionic-native/status-bar';
 
-import { MyApp } from './app.component';
-import { HomePage } from '../pages/home/home';
+import {MyApp} from './app.component';
+import {HttpClientModule} from "@angular/common/http";
+import {HomePageModule} from "../pages/home/home.module";
+
+
+import {AndroidPermissions} from '@ionic-native/android-permissions';
+import {Geolocation} from "@ionic-native/geolocation";
+import { LocationserveProvider } from '../providers/locationserve/locationserve';
+import {Diagnostic} from "@ionic-native/diagnostic";
+import {LocationAccuracy} from "@ionic-native/location-accuracy";
+import {BackgroundGeolocation} from "@ionic-native/background-geolocation";
 
 @NgModule({
   declarations: [
-    MyApp,
-    HomePage
+    MyApp
   ],
   imports: [
     BrowserModule,
+    HttpClientModule,
+    HomePageModule,
     IonicModule.forRoot(MyApp)
   ],
   bootstrap: [IonicApp],
   entryComponents: [
-    MyApp,
-    HomePage
+    MyApp
   ],
   providers: [
+    AndroidPermissions,
+    Geolocation,
+    Diagnostic,
+    LocationAccuracy,
+    BackgroundGeolocation,
     StatusBar,
     SplashScreen,
-    {provide: ErrorHandler, useClass: IonicErrorHandler}
+    {provide: ErrorHandler, useClass: IonicErrorHandler},
+    LocationserveProvider
   ]
 })
-export class AppModule {}
+export class AppModule {
+}
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,75 @@
-import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import {Component} from '@angular/core';
+import {IonicPage, NavController, NavParams, Platform} from 'ionic-angular';
+import {LocationserveProvider} from "../../providers/locationserve/locationserve";
 
+
+@IonicPage()
 @Component({
   selector: 'page-home',
-  templateUrl: 'home.html'
+  templateUrl: 'home.html',
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  data: any = "STARTED HOME PAGE";
 
+  constructor(public navCtrl: NavController, public navParams: NavParams, private locationServe: LocationserveProvider, private platform: Platform) {
+  }
+
+
+  ionViewDidLoad() {
+
+  }
+
+  async requestPermisson() {
+    this.platform.ready().then(async (readySource) => {
+      await this.locationServe.getPermission();
+    }).catch(err => {
+      alert("Platform Error :" + JSON.stringify(err));
+    });
+  }
+
+  async startForegroundTime() {
+    this.platform.ready().then(async (readySource) => {
+      this.data = await this.locationServe.startForegroundTimeout();
+    }).catch(err => {
+      alert("Platform Error :" + JSON.stringify(err));
+    });
+  }
+
+
+  async startForegroundNoTime() {
+    this.platform.ready().then(async (readySource) => {
+       await this.locationServe.getLocationAccuracy();
+
+       this.data = await this.locationServe.startForegroundNoTimeout();
+
+    }).catch(err => {
+      alert("Platform Error :" + JSON.stringify(err));
+    });
+  }
+
+  async startBackground() {
+    this.platform.ready().then(async (readySource) => {
+      this.data = await this.locationServe.startBackGroundLocationTracking();
+    }).catch(err => {
+      alert("Platform Error :" + JSON.stringify(err));
+    });
+  }
+
+  async startBackgroundForeground() {
+    this.platform.ready().then(async (readySource) => {
+      this.data = await this.locationServe.startBackGroundLocationTrackingForeground();
+    }).catch(err => {
+      alert("Platform Error :" + JSON.stringify(err));
+    });
+  }
+
+  async stopBackground() {
+    this.platform.ready().then(async (readySource) => {
+      await this.locationServe.stopBackGroundLocationTracking();
+    }).catch(err => {
+      alert("Platform Error :" + JSON.stringify(err));
+    });
   }
 
 }
